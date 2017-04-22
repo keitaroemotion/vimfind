98342e4137bfbce4e073da9db97c96abf812210c
kei <kei@omise.co>
2017-04-22 15:29:41 +0900

    refactoring: has?

--- lib/util.rb
+++ lib/util.rb
@@ -1,9 +1,21 @@
 class Util
-  def self.gsubs(x, kws)
-    kws.split(" ").each do |kw|
-      x = x.gsub(kw, kw.green)
+  def self.gsubs(text, keywords)
+    keywords.split(" ").each do |keyword|
+      text = text.gsub(keyword, keyword.green)
     end
-    x
+    text
+  end
+
+  def self.filter_tail(array, text)
+    array.select {|member| member.end_with?(text) }
+  end
+
+  def self.has?(x, kws)
+    kws = kws.split(" ")
+    negs = kws.select {|kw| kw.start_with?("!") }.map{|kw| kw.gsub("!", "")}
+    kws = kws.select {|kw| !kw.start_with?("!") }
+    kws.select{|kw| x.include?(kw)}.size == kws.size && \
+      negs.select{|kw| x.include?(kw)}.size == 0
   end
 end  
 
